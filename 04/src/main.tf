41fa9f0e70b1fd9b3e1c2f5489a9ce25e44603fc
ShulgatiySV <megasts@yandex.ru>
2024-09-16 22:35:04 +0500

    v0.4

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -1,10 +1,61 @@
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
-resource "yandex_vpc_subnet" "develop" {
+resource "yandex_vpc_subnet" "develop_a" {
   name           = var.vpc_name
-  zone           = var.default_zone
+  zone           = var.zone_a
   network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
+  v4_cidr_blocks = var.cidr_zone_a
 }
 
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = var.vpc_name
+  zone           = var.zone_b
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.cidr_zone_b
+}
+
+module "marketing" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop" 
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = [var.zone_a]
+  subnet_ids     = [yandex_vpc_subnet.develop_a.id]
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = { 
+    owner= "megion",
+    project = "marketing"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
+  }
+
+}
+
+module "analytics" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "stage"
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = [var.zone_a]
+  subnet_ids     = [yandex_vpc_subnet.develop_a.id]
+  instance_name  = "analytics"
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = { 
+    owner= "megion",
+    project = "analytics"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
+  }
+
+}
--- 04/src/variables.tf
+++ 04/src/variables.tf
@@ -19,14 +19,36 @@ variable "folder_id" {
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
-variable "default_zone" {
+# variable "default_zone" {
+#   type        = string
+#   default     = "ru-central1-a"
+#   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+# }
+
+variable "zone_a" {
   type        = string
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
-variable "default_cidr" {
+variable "zone_b" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+# variable "default_cidr" {
+#   type        = list(string)
+#   default     = ["10.0.1.0/24"]
+#   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+# }
+
+variable "cidr_zone_a" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+variable "cidr_zone_b" {
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
